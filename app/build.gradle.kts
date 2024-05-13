f8d45692e31970b40586d4c1c7bfdc49e83e8811
ho-taek <kht072623@naver.com>
2024-05-13 21:18:18 +0900

    chore(#1) : 라이브러리 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,41 @@ android {
 
 dependencies {
 
+  //androidx
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.12.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("androidx.fragment:fragment-ktx:1.3.6")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+
+
+  //test
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+  // navigation
+  implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+  implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+
+  // retrofit,
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+  //okHttpClient
+  implementation("com.squareup.okhttp3:okhttp:4.9.0")
+  implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+  // gilde
+  implementation("com.github.bumptech.glide:glide:4.12.0")
+  annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+  // timber
+  implementation("com.jakewharton.timber:timber:4.7.1")
+
+  // tedPermission
+  implementation("io.github.ParkSangGwon:tedpermission-normal:3.3.0")
+  implementation("io.github.ParkSangGwon:tedpermission-coroutine:3.3.0")
 }
