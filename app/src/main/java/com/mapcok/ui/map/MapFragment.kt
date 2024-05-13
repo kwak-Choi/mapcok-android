95ba43698204853ca997ae551b80e238f469418e
csjcozy <tmdwns63@gmail.com>
2024-05-14 02:34:03 +0900

    feat{#3} 맵 구현
    java.lang.UnsatisfiedLinkError: dlopen failed: library "libK3fAndroid.so" not found 오류로 해결중

--- app/src/main/java/com/mapcok/ui/map/MapFragment.kt
+++ app/src/main/java/com/mapcok/ui/map/MapFragment.kt
@@ -1,21 +1,44 @@
 package com.mapcok.ui.map
 
+import android.os.Bundle
+import android.view.View
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 import com.mapcok.R
 import com.mapcok.databinding.FragmentMapBinding
 import com.mapcok.ui.base.BaseFragment
+import java.lang.Exception
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+    private lateinit var mapView:MapView
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = binding.mapView
+
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+            }
+
+            override fun onMapError(p0: Exception?) {
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(p0: KakaoMap) {
+                binding.mapView.addView(mapView)
+            }
+        })
+
+
+    }
     override fun initView() {
-        binding.mapView.start(MapLifeCycleCallback()){
-            o
-        }
+
+
     }
 
     override fun onResume() {
         super.onResume()
-        binding.mapView.resume()
+        mapView.resume()
     }
 
     override fun onPause() {
