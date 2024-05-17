2aedaa8d90fe486f9e8259edd0558842808aa466
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-05-17 18:08:36 +0900

    ui(#18) : 유저 리스트

---
+++ app/src/main/java/com/mapcok/ui/userlist/UserListFragment.kt
@@ -0,0 +1,14 @@
+package com.mapcok.ui.userlist
+
+import com.mapcok.R
+import com.mapcok.databinding.FragmentUserListBinding
+import com.mapcok.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserListFragment : BaseFragment<FragmentUserListBinding>(R.layout.fragment_user_list) {
+  override fun initView() {
+
+  }
+
+}
---
+++ app/src/main/java/com/mapcok/ui/userlist/adapter/UserListAdapter.kt
@@ -0,0 +1,16 @@
+package com.mapcok.ui.userlist.adapter
+
+import android.service.autofill.UserData
+import androidx.recyclerview.widget.ListAdapter
+import com.mapcok.databinding.UserListItemBinding
+
+class UserListAdapter : ListAdapter<UserData> {
+
+
+
+
+
+  class UserListViewHolder(
+    val binding : UserListItemBinding
+  )
+}
